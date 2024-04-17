e6d584c67d068590aa5d0d7b48e7cb3a98d92eda
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-04-17 18:13:48 +0900

    feat: Bullet UI

--- Assets/01_Scripts/VCS/EntityManage/Enemy/Enemy.cs
+++ Assets/01_Scripts/VCS/EntityManage/Enemy/Enemy.cs
@@ -1,16 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace EntityManage.Enemy
 {
     public abstract class Enemy : Entity
     {
-        
         public Animator AnimatorCompo { get; private set; }
-
         public bool CanStateChangeable { get; private set; }
-        
-        
-        
-        
+
+        protected virtual void Awake()
+        {
+            AnimatorCompo = GetComponentInChildren<Animator>();
+        }
+
+        protected abstract void AttackPlayer();
     }
 }
