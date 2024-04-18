08c746f8c697ac2270499d2003803f5995898718
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-04-18 22:16:07 +0900

    fix: EnemyWeapon

---
+++ Assets/01_Scripts/Gominsu/Enemy/BombVest/EnemyWeapon.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyWeapon : MonoBehaviour
+{
+
+    public int damage = 0;//데미지
+
+    public bool reloadCheck = true;//재장전 체크
+
+    public int maxBulletCount = 0;//총탄수
+
+    public int currentBulletCount = 0;
+
+    public float fireDelay;//연사 속도
+
+    public float reloadTime;//재장전 시간
+
+    public float bulletSpeed;//총알 스피드
+
+    public float destroyRange;//사정거리
+
+    public EnemyWeaponSO enemyWeaponSO;
+
+    public GameObject bulletPrefab;//총얼 프리펩
+
+    public Transform gunTip;//총구 위치
+    public abstract void Fire(Vector2 direction);
+
+    protected virtual void Awake()
+    {
+        gunTip = transform.Find("GunTip");//총구의치 받아옴
+        fireDelay = enemyWeaponSO.fireDelay;
+        damage = enemyWeaponSO.damage;
+        maxBulletCount = enemyWeaponSO.maxBulletCount;
+        reloadTime = enemyWeaponSO.reloadTime;
+        bulletSpeed = enemyWeaponSO.bulletSpeed;
+        destroyRange = enemyWeaponSO.destroyRange;
+        bulletPrefab = enemyWeaponSO.bulletPrefab;
+        currentBulletCount = maxBulletCount;
+
+
+    }
+
+    public virtual void Reload()
+    {
+        StartCoroutine(ReloadCoroutine());
+    }
+
+    protected virtual IEnumerator ReloadCoroutine()
+    {
+        reloadCheck = false;
+        yield return new WaitForSeconds(reloadTime);//reloadTime만큼 기다렸다가 밑에 코드를 실행
+        reloadCheck = true;
+        currentBulletCount = maxBulletCount;
+    }
+
+
+}
