337a4bff68858fb7f6c28d1f87aa188b5ee49e8b
SundG0162 <123521471+SundG0162@users.noreply.github.com>
2024-05-02 19:34:15 +0900

    Feat : MapChange

--- Assets/01_Scripts/HW/MapManager.cs
+++ Assets/01_Scripts/HW/MapManager.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class MapManager : MonoSingleton<MapManager>
 {
+    [SerializeField]
     private StageSO _stageSO;
 
     private GameObject _currentStagePrefab;
 
-    private int _currentStageIndex = 0;
-
-    public void NextStage()
+    public Transform NextStage()
     {
         Destroy(_currentStagePrefab);
-        _currentStageIndex++;
-        _currentStagePrefab = Instantiate(_stageSO.stagePrefab[_currentStageIndex]);
+        int rand = Random.Range(0, _stageSO.stagePrefabs.Count);
+        _currentStagePrefab = Instantiate(_stageSO.stagePrefabs[rand]);
+        return _currentStagePrefab.transform;
     }
 }
