49c74728e65d515c8a056252d5d16dcbe88785e7
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-05-04 03:05:14 +0900

    feat: EnemyManager GenerateEnemys

--- Assets/01_Scripts/VCS/EnemyManager.cs
+++ Assets/01_Scripts/VCS/EnemyManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyManager : MonoSingleton<EnemyManager>
 {
@@ -11,12 +13,24 @@ public class EnemyManager : MonoSingleton<EnemyManager>
 
     public int AliveEnemy => _enemyList.Count;
 
-    public void GenerateEnemy(Transform[] SpawnPosTrms)
+    private void Start()
+    {
+        MapManager.Instance.OnStageChange += GenerateEnemy;
+    }
+
+    private void OnDisable()
+    {
+        MapManager.Instance.OnStageChange -= GenerateEnemy;
+    }
+
+    public void GenerateEnemy()
     {
         foreach (Enemy e in _enemyList)
         {
             Destroy(e.gameObject);
         }
+
+        Transform[] SpawnPosTrms = MapManager.Instance.GetEnemyPositions();
         _enemyList = new List<Enemy>();
         foreach (Transform trm in SpawnPosTrms)
         {
