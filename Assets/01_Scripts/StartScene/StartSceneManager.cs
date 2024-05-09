fab1b86d64ad77df1f048863786e40288eea4792
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-05-09 19:43:43 +0900

    feat: PlayDataLog

--- Assets/01_Scripts/StartScene/StartSceneManager.cs
+++ Assets/01_Scripts/StartScene/StartSceneManager.cs
@@ -5,11 +5,24 @@ namespace StartScene
 {
     public class StartSceneManager : MonoBehaviour
     {
-        
+        [SerializeField] private SpriteRenderer _spriteRenderer;
 
+        private int clickAmount = 0;
+        private float _alpha = 0;
         public void LoadInGameScene()
         {
             SceneManager.LoadScene("00_Scenes/InGameScene");
+        }
+
+        public void ClickStartCount()
+        {
+            clickAmount++;
+            if (clickAmount >= 5)
+            {
+                _alpha += 0.01f;
+                clickAmount = 0;
+                _spriteRenderer.color = new Color(1,1,1,_alpha);
+            }
         }
 
         public void Quit()
---
+++ Assets/01_Scripts/VCS/UI/LogUIManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class LogUIManager : MonoBehaviour
+{
+    [SerializeField] private LogUI _logUIPrefab;
+    [SerializeField] private Transform _contentTrm;
+    [SerializeField] private PlayDataLog _log;
+
+    private void Awake()
+    {
+        _log = DBManager.GetPlayLog();
+        RefreshLog();
+    }
+
+    private void RefreshLog()
+    {
+        foreach (Transform child in _contentTrm)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (PlayData data in _log.playDataLog)
+        {
+            LogUI log = Instantiate(_logUIPrefab, _contentTrm);
+            log.SetLog(data);
+        }
+    }
+}
